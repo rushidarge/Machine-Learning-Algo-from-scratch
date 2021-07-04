1f3b66201c0ff4b16f3422805335aba909f3620a
Rushikesh Darge <39642887+rushidarge@users.noreply.github.com>
2021-07-04 08:23:12 +0530

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,58 @@
+# Importing libraries for mathematical calculations
+import numpy as np
+from collections import Counter
+
+# to find out distance between two points
+def euclidean_dist(x1,x2):
+    return np.sqrt(np.sum((x1-x2)**2))
+
+class KNN:
+
+    def __init__(self,k=3):
+        '''
+        Parameters: 
+            k=3 Number of nearest neighbour
+
+        Return None
+        '''
+        # defin k for nearest neighbour 
+        self.k = k
+
+    def fit(self,X,y):
+        '''
+        Parameters:
+            X training features
+            y training labels
+
+        Return None
+        '''
+        # save x,y for distance calculation 
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self,X):
+        '''
+        Parameters:
+            X dataset that you want to predict the values
+
+        Return Prediction on your dataset
+        '''
+        # return the prediction
+        predicted_out = [self._predict(x) for x in X]
+        return predicted_out
+
+    def _predict(self,x):
+        # Internal function to calculate distance, find nearest neighbours and return label
+        dist_x = [euclidean_dist(x,X_tr) for X_tr in self.X_train]
+        points = [np.argsort(dist_x)[:self.k]]
+        y_pre = [self.y_train[i] for i in points]
+        ret_y = Counter(y_pre[0]).most_common(1)
+        return ret_y[0][0]
+
+    def score(self):
+        '''
+        Return
+            Accuracy score of your model on train dataset
+        '''
+        pre = self.predict(self.X_train)
+        print(np.sum(self.y_train==pre)/len(self.y_train))
