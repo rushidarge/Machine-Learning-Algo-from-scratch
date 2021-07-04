38ca00e8de0a812be9c027ffb6ea81fa78768a0c
Rushikesh Darge <39642887+rushidarge@users.noreply.github.com>
2021-07-04 08:28:07 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -56,3 +56,20 @@ class KNN:
         '''
         pre = self.predict(self.X_train)
         print(np.sum(self.y_train==pre)/len(self.y_train))
+        
+#  Implementation Code       
+'''
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+iris = datasets.load_iris()
+X = iris['data']
+y = iris['target']
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+
+
+clf = KNN(k=5)
+clf.fit(X_train,y_train)
+prediction = clf.predict(X_test)
+clf.score()
+'''
